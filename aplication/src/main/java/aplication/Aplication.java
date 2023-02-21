f030abf8ebbe44a9e61a1f1b3382dacde1213a43
rej <Rayearth777@wp.pl>
2023-02-21 21:05:54 +0100

    dodanie CSS Mesage Less gurna belka z linkami

--- aplication/src/main/java/aplication/Aplication.java
+++ aplication/src/main/java/aplication/Aplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 import pet_clinic_web.ComonUtil1;
 import pet_clinic_web.ThymeleafConfiguration;
