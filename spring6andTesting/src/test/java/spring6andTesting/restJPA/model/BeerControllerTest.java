bf018a1d69d84c675d1953b23d4dd9a51e8cc8fa
rej <Rayearth777@wp.pl>
2024-06-25 11:35:52 +0200

    fix for testDeleteBeer

--- spring6andTesting/src/test/java/spring6andTesting/restJPA/model/BeerControllerTest.java
+++ spring6andTesting/src/test/java/spring6andTesting/restJPA/model/BeerControllerTest.java
@@ -101,7 +101,9 @@ class BeerControllerTest {
     void testDeleteBeer() throws Exception {
         BeerDTO beer = beerServiceImpl.listBeers().get(0);
 
-        mockMvc.perform(delete(BeerController.BEER_PATH + "/"+ beer.getId())
+        given(beerService.deleteById(any())).willReturn(true);
+
+        mockMvc.perform(delete(BeerController.BEER_PATH_ID, beer.getId())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
 
